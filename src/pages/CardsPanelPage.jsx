46a607fcfc37407d44174e0c60c2dfce380d4071
Rlss91 <rlss91@gmail.com>
2022-04-03 11:49:24 +0300

    ternery cards panel

--- src/pages/CardsPanelPage.jsx
+++ src/pages/CardsPanelPage.jsx
@@ -1,9 +1,47 @@
+import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+import CardComponent from "../components/CardComponent/CardComponent";
 
 const CardsPanelPage = () => {
   const location = useLocation();
+  const [cardsArr, setCardsArr] = useState([]);
+  const [objID, setObjID] = useState("");
   console.log("location.pathname", location.pathname);
-  return <h1>CardsPanel Page</h1>;
+  useEffect(() => {
+    const objIdFromStorage = localStorage.getItem("tokenKey");
+    const decoded = jwt_decode(objIdFromStorage);
+    console.log("decoded", decoded);
+    setObjID(decoded._id);
+    axios
+      .get("/cards/allCards")
+      .then(({ data }) => {
+        setCardsArr(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return (
+    <div className="row row-cols-1 row-cols-md-2 g-4">
+      {cardsArr.map((item) => {
+        return (
+          <CardComponent
+            id={item._id}
+            key={item._id}
+            name={item.name}
+            description={item.description}
+            phone={item.phone}
+            address={item.address}
+            image={item.image}
+            userIDCard={item.userID}
+            userIDLoggedIn={objID}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default CardsPanelPage;
